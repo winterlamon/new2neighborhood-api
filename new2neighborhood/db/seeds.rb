6c1e54f6a80911df0d113ffc112ca18a527450f6
github username <github email address>
2017-12-20 17:06:24 -0500

    added Foursquare2 client code

--- new2neighborhood/db/seeds.rb
+++ new2neighborhood/db/seeds.rb
@@ -12,8 +12,19 @@ User.create(first_name: "Winter", last_name: "LaMon", street: "11 Broadway", cit
 # Client Secret
 # Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30
 
-client = Foursquare2::Client.new(:client_id => '1XA3FP3VEHXNXDJSA1GMNDARTHHSZ2KJ5PLHKW0MBM50MFX0', :client_secret => 'Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30')
+client = Foursquare2::Client.new(:client_id => '1XA3FP3VEHXNXDJSA1GMNDARTHHSZ2KJ5PLHKW0MBM50MFX0', :client_secret => 'Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30', :api_version => '20171220')
 
+# venue = client.venue(1)
+# venue.location.formattedAddress
+# venue.location.formattedAddress[0] = "street address (crossStreet)"
+# venue.location.formattedAddress[1] = "city, state, postalCode"
+# venue.location.formattedAddress[2] = "country"
+
+# x = client.search_venues(:ll => lat, long)
+# x.location.address => address, city
+# x.location.postalCode
+
+return client
 
 
 
