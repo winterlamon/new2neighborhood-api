959c2f7f316a009bc477b14199918e62e6c1ee33
github username <github email address>
2017-12-20 17:34:34 -0500

    added call to api

--- new2neighborhood/app/controllers/venues_controller.rb
+++ new2neighborhood/app/controllers/venues_controller.rb
@@ -1,2 +1,17 @@
 class VenuesController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+        venues = Venue.call_api
+        venues.each do |venue| 
+            Venue.new
+            Venue.name = venue.name
+            Venue.save
+    end
+
 end
--- new2neighborhood/db/seeds.rb
+++ new2neighborhood/db/seeds.rb
@@ -12,8 +12,8 @@ User.create(first_name: "Winter", last_name: "LaMon", street: "11 Broadway", cit
 # Client Secret
 # Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30
 
-client = Foursquare2::Client.new(:client_id => '1XA3FP3VEHXNXDJSA1GMNDARTHHSZ2KJ5PLHKW0MBM50MFX0', :client_secret => 'Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30', :api_version => '20171220')
-
+#client = Foursquare2::Client.new(:client_id => '1XA3FP3VEHXNXDJSA1GMNDARTHHSZ2KJ5PLHKW0MBM50MFX0', :client_secret => 'Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30', :api_version => '20171220')
+# Our location: :ll => '40.704069,-74.0132413'
 # venue = client.venue(1)
 # venue.location.formattedAddress
 # venue.location.formattedAddress[0] = "street address (crossStreet)"
@@ -23,8 +23,6 @@ client = Foursquare2::Client.new(:client_id => '1XA3FP3VEHXNXDJSA1GMNDARTHHSZ2KJ
 # x = client.search_venues(:ll => lat, long)
 # x.location.address => address, city
 # x.location.postalCode
-
-return client
 
 
 
