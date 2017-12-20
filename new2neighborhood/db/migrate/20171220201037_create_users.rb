60bafc5aa536b04e6963d90aaf546708d668712d
github username <github email address>
2017-12-20 15:13:30 -0500

    create rails app

---
+++ new2neighborhood/db/migrate/20171220201037_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :neighborhood
+
+      t.timestamps
+    end
+  end
+end
---
+++ new2neighborhood/db/migrate/20171220201300_create_venues.rb
@@ -0,0 +1,13 @@
+class CreateVenues < ActiveRecord::Migration[5.1]
+  def change
+    create_table :venues do |t|
+      t.string :name
+      t.string :location
+      t.string :categories
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
