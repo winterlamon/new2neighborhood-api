6e98fe84cc6661aa74aa2d274dcce2ed19331fc5
github username <github email address>
2017-12-27 14:04:21 -0500

    can search by food or drink

--- new2neighborhood/app/models/venue.rb
+++ new2neighborhood/app/models/venue.rb
@@ -1,18 +1,18 @@
 class Venue < ApplicationRecord
 
-  def self.call_api(lat, lon)
+  def self.call_api(lat, lon, radius, section)
     date = self.get_date
     client = Foursquare2::Client.new(:client_id => '1XA3FP3VEHXNXDJSA1GMNDARTHHSZ2KJ5PLHKW0MBM50MFX0', 
             :client_secret => 'Y5ETOZQBBOZA5WJHAQEGNLIVBEGD0BGRTSZXUQPCBXQD0C30', 
             :api_version => date)
     ll = lat + ',' + lon
-    client.search_venues(:ll => ll, :query => 'bar+restaurant', :limit => 50).venues
+    client.explore_venues(:ll => ll, :radius=> radius, :section=> section, :limit => 100).groups[0]
   end
 
-  def self.create_from_location(lat = '40.704069', lon = '-74.0132413')
+  def self.create_from_location(lat = '40.704069', lon = '-74.0132413', radius='1000', section='food')
     created_venues = []
     venue_data = self.call_api(lat, lon)
-    venue_data.each do |data| 
+    venue_data.items.each do |data|
       venue = Venue.find_or_create_by(
         name: data.name,
         address:  data.location.address,
@@ -35,6 +35,7 @@ class Venue < ApplicationRecord
     year = Date.today.year
     month = Date.today.month
     day = Date.today.day
-    today = [year, month, day].join('')
+    date = [year, month, day].join('')
+    date
   end
 end
