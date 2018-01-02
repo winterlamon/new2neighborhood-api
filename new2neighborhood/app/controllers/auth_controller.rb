b0f607c6da0c07b0a9daa8e76a2e6127281e6c0c
github username <winter.lamon@gmail.com>
2018-01-02 15:06:00 -0500

    added keys on user render

--- new2neighborhood/app/controllers/auth_controller.rb
+++ new2neighborhood/app/controllers/auth_controller.rb
@@ -17,7 +17,7 @@ class AuthController < ApplicationController
         # Requires headers to contain 'Authorization': token
         if current_user
             user = current_user
-            render json: {username: user.email, id: user.id}
+            render json: {id: user.id, first_name: user.first_name, last_name: user.last_name, username: user.email, venues: user.venues, token: issue_token({id: user.id})}
         else
             render json: {error: "Invalid token"}, status: 401
         end
